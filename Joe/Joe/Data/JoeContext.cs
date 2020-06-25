aecf626d67757ccf1439b9af5c61cfe812fdc74d
JosephSteelePerkins2 <josephsteeleperkinsjosephsteeleperkins>
2020-06-25 16:29:27 +0100

    web stuff

---
+++ Joe/Joe/Data/JoeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Joe.Models;
+
+namespace Joe.Data
+{
+    public class JoeContext : DbContext
+    {
+        public JoeContext (DbContextOptions<JoeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Joe.Models.Movie> Movie { get; set; }
+    }
+}
