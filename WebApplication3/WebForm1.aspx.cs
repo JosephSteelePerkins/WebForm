34d2999cb9fa96b316e4211462e0072ebee999d4
Joseph Steele-Perkins <joseph_steeleperkins@yahoo.co.uk>
2020-06-30 19:00:21 +0100

    new solution that actually works

---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Data.SqlClient;
+
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection SQLConn = new SqlConnection("Persist Security Info = False; Integrated Security = true; Initial Catalog = Books; server = (local)");
+            SqlDataAdapter SQLAdaptor = new SqlDataAdapter("select * from dbo.Book", SQLConn);
+            DataTable DT = new DataTable();
+            SQLAdaptor.Fill(DT);
+            GridView1.DataSource = DT;
+            GridView1.DataBind();
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
